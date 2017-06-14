4b5bd35073a167fea1a43d57d6d4a5d4ad91fcdf
Sachielsc <sachielsc@gmail.com>
2017-06-14 16:08:37 +1200

    heroku final

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var port = process.env.PORT || 8080
 app.use(express.static(__dirname));
 
 // routes
-app.get("/webapp", function(req, res) {
+app.get("/", function(req, res) {
 	res.render("index");
 })
 
