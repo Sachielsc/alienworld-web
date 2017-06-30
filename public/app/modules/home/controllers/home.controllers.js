8e0698d007a5616acf4b1a2c43e37245a50903e6
Sachielsc <sachielsc@gmail.com>
2017-06-30 16:46:53 +1200

    friday 1st

--- public/app/modules/home/controllers/home.controllers.js
+++ public/app/modules/home/controllers/home.controllers.js
@@ -1,9 +1,5 @@
 'use strict';
 angular.module('home').controller('homeController', ['$log', '$scope', function($log, $scope){
-	$scope.currentdate =  new Date();
-	$scope.currentdate.year = $scope.currentdate.getFullYear();
-	$scope.currentdate.month = $scope.currentdate.getMonth() + 1;
-	$scope.currentdate.day = $scope.currentdate.getDate();
-	$scope.currentdate.date = $scope.currentdate.day + "/" + $scope.currentdate.month + "/" + $scope.currentdate.year;
+
 }]
 )
