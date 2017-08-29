dc17e56315f634c38869e074cb0f933f4d2792fc
Sachielsc <sachielsc@gmail.com>
2017-08-29 17:49:44 +1200

    minor

--- server.js
+++ server.js
@@ -2,19 +2,19 @@ var express = require('express');
 var app = express();
 
 // set port
-var port = process.env.PORT || 8080;
+var port = process.env.PORT || 5000;
 
 app.use(express.static(__dirname + '/public'));
 
 // routes
-app.get("/", function(req, res) {
-	res.render("index");
+app.get("/", function(request, response) {
+	response.render("index");
 });
 
-app.use(function(req, res) {
-    res.sendfile(__dirname + '/public/index.html');
+app.use(function(request, response) {
+    response.sendfile(__dirname + '/public/index.html');
 });
 
 app.listen(port, function() {
-	console.log("app running");
+	console.log("listening to this port:" + port);
 });
