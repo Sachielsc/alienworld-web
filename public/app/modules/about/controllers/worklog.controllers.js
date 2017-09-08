8c6ee892142655535c7a6b15538a271569dbde40
Sachielsc <sachielsc@gmail.com>
2017-09-08 16:25:05 +1200

    add password function to worklog module

---
+++ public/app/modules/about/controllers/worklog.controllers.js
@@ -0,0 +1,20 @@
+'use strict';
+angular.module('about').controller('worklogController', ['$log', '$scope', function($log, $scope){
+
+    var myPassWord = "scsgdtcy3";
+    
+    $scope.hideWorkLog = true;
+
+    $scope.clearValueWorkLog = function () {
+        $scope.coverletterpassword = "";
+    };
+
+    $scope.passValueWorkLog = function () {
+        if ($scope.worklogpassword == myPassWord) {
+        	$scope.hideWorkLog = false;
+        } else {
+        	$scope.hideWorkLog = true;
+        }
+    };
+}]
+)
