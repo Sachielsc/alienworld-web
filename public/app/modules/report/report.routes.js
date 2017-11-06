4969339f750b1ba7840dd5a9f3933850abf3514d
Sachielsc <sachielsc@gmail.com>
2017-11-07 11:19:59 +1300

    1. add asp.net work reports 2. fix bugs in routers

--- public/app/modules/report/report.routes.js
+++ public/app/modules/report/report.routes.js
@@ -4,7 +4,7 @@ angular.module('report').config(['$stateProvider', function($stateProvider) {
     console.log('report routers loading complete...');
 
     // add my own study notes
-    var studyNoteList = ["noteNodeJS", "angularJS", "html", "bootstrap", "generalCoding", "generalIT", "vocabulary", "java", "vim", "jira", "reactJS", "linux", "gulp", "sublime", "git", "heroku", "aspnet"];
+    var studyNoteList = ["nodeJS", "angularJS", "html", "bootstrap", "generalCoding", "generalIT", "vocabulary", "java", "vim", "jira", "reactJS", "linux", "gulp", "sublime", "git", "heroku", "aspnet", "IntelliJIDEA"];
 
     for (var j = 0; j < studyNoteList.length; j++) {
         var studyNoteStateName = "workreport." + studyNoteList[j];
